5631c01f1901b828b1bf319589a766c28f567125
rjuance <rjuance@cs.binghamton.edu>
2025-08-28 12:32:48 -0400

    finished lab

--- zoo.cpp
+++ zoo.cpp
@@ -1,34 +1,23 @@
+#include "Animal.h"
+#include "AnimalsInZoo.h"
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
-class Animal {
-   public:
-      Animal(string speciesName, unsigned int discoveryYear) {
-         species = speciesName;
-         year_discovered = discoveryYear;
-      }
-
-      Animal() : species(""), year_discovered(0) {};
-
-      void display() {
-         cout << species << " [" << year_discovered << "]" << endl;
-      }
-
-   private:
-      string species = "";
-      unsigned int year_discovered = 0;
-};
-
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
+   Animal animal3("Polar Bear", 1774);
 
    delete animal1;
    animal1 = new Animal("Snow Leopard", 1777);
 
+   animal3.display();
    animal2.display();
    animal1->display();
 
    delete animal1;
+
+   AnimalsInZoo test1(animal2);
+   test1.display();
 }
